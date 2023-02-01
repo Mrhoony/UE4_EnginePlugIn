19777a2d7d2b69af3817dbc067001386f9aad1f0
TESTID <52897037+Mrhoony@users.noreply.github.com>
2023-02-01 16:50:08 +0900

    Create SliceCube

--- Source/Game/Objects/CVertexMesh.cpp
+++ Source/Game/Objects/CVertexMesh.cpp
@@ -4,14 +4,96 @@
 ACVertexMesh::ACVertexMesh()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.TickInterval = 1.f;
 
 	ProcMesh = CreateDefaultSubobject<UProceduralMeshComponent>("ProcMesh");
 	RootComponent = ProcMesh;
 
-	// Define Vertex Desc
+	FVector v = FVector(0.5f);
 
-	// IASet -> DrawPrimitiveCalss
-	// DC->Draw
+	// Front
+	Positions.Add(FVector(-0.5f, -0.5f, -0.5f)); UVs.Add(FVector2D(0, 1));
+	Positions.Add(FVector(-0.5f, -0.5f, +0.5f)); UVs.Add(FVector2D(0, 0));
+	Positions.Add(FVector(-0.5f, +0.5f, -0.5f)); UVs.Add(FVector2D(1, 1));
+	Positions.Add(FVector(-0.5f, +0.5f, +0.5f)); UVs.Add(FVector2D(1, 0));
+
+	for (int32 i = 0; i < 4; i++)
+	{
+		Normals.Add(FVector(-1, 0, 0));
+		Colors.Add(FColor::Red);
+	}
+	AddIndices(0); // AddIndices(Indices.Num());
+	//Back
+	Positions.Add(FVector(+v.X, +v.Y, -v.Z)); UVs.Add(FVector2D(0, 1));
+	Positions.Add(FVector(+v.X, +v.Y, +v.Z)); UVs.Add(FVector2D(0, 0));
+	Positions.Add(FVector(+v.X, -v.Y, -v.Z)); UVs.Add(FVector2D(1, 1));
+	Positions.Add(FVector(+v.X, -v.Y, +v.Z)); UVs.Add(FVector2D(1, 0));
+
+	for (int32 i = 0; i < 4; i++)
+	{
+		Normals.Add(FVector(+1, 0, 0));
+		Colors.Add(FColor::Green);
+	}
+	AddIndices(4);
+
+
+	//Top
+	Positions.Add(FVector(-v.X, -v.Y, +v.Z)); UVs.Add(FVector2D(0, 1));
+	Positions.Add(FVector(+v.X, -v.Y, +v.Z)); UVs.Add(FVector2D(0, 0));
+	Positions.Add(FVector(-v.X, +v.Y, +v.Z)); UVs.Add(FVector2D(1, 1));
+	Positions.Add(FVector(+v.X, +v.Y, +v.Z)); UVs.Add(FVector2D(1, 0));
+
+	for (int32 i = 0; i < 4; i++)
+	{
+		Normals.Add(FVector(0, 0, +1));
+		Colors.Add(FColor::Blue);
+	}
+	AddIndices(8);
+
+
+	//Bottom
+	Positions.Add(FVector(-v.X, -v.Y, -v.Z)); UVs.Add(FVector2D(0, 1));
+	Positions.Add(FVector(-v.X, +v.Y, -v.Z)); UVs.Add(FVector2D(1, 1));
+	Positions.Add(FVector(+v.X, -v.Y, -v.Z)); UVs.Add(FVector2D(0, 0));
+	Positions.Add(FVector(+v.X, +v.Y, -v.Z)); UVs.Add(FVector2D(1, 0));
+
+	for (int32 i = 0; i < 4; i++)
+	{
+		Normals.Add(FVector(0, 0, -1));
+		Colors.Add(FColor::Cyan);
+	}
+	AddIndices(12);
+
+
+	//Left
+	Positions.Add(FVector(+v.X, -v.Y, -v.Z)); UVs.Add(FVector2D(0, 1));
+	Positions.Add(FVector(+v.X, -v.Y, +v.Z)); UVs.Add(FVector2D(0, 0));
+	Positions.Add(FVector(-v.X, -v.Y, -v.Z)); UVs.Add(FVector2D(1, 1));
+	Positions.Add(FVector(-v.X, -v.Y, +v.Z)); UVs.Add(FVector2D(1, 0));
+
+	for (int32 i = 0; i < 4; i++)
+	{
+		Normals.Add(FVector(0, -1, 0));
+		Colors.Add(FColor::Magenta);
+	}
+	AddIndices(16);
+
+
+	//Right
+	Positions.Add(FVector(-v.X, +v.Y, -v.Z)); UVs.Add(FVector2D(0, 1));
+	Positions.Add(FVector(-v.X, +v.Y, +v.Z)); UVs.Add(FVector2D(0, 0));
+	Positions.Add(FVector(+v.X, +v.Y, -v.Z)); UVs.Add(FVector2D(1, 1));
+	Positions.Add(FVector(+v.X, +v.Y, +v.Z)); UVs.Add(FVector2D(1, 0));
+
+	for (int32 i = 0; i < 4; i++)
+	{
+		Normals.Add(FVector(0, +1, 0));
+		Colors.Add(FColor::Yellow);
+	}
+	AddIndices(20);
+
+	ProcMesh->CreateMeshSection(0, Positions, Indices, Normals, UVs, Colors, TArray<FProcMeshTangent>(), true);
+	ProcMesh->SetRelativeScale3D(FVector(100));
 }
 
 void ACVertexMesh::BeginPlay()
@@ -22,9 +104,34 @@ void ACVertexMesh::BeginPlay()
 void ACVertexMesh::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	for (int32 i = 0; i < Colors.Num() / 4; i++)
+	{
+		FColor color = FColor::MakeRandomColor();
+		color.A = 255.f;
+
+		Colors[i * 4 + 0] = color;
+		Colors[i * 4 + 1] = color;
+		Colors[i * 4 + 2] = color;
+		Colors[i * 4 + 3] = color;
+
+		//Positions[i * 4 + 0] += FVector(0, -0.5, -0.5) * DeltaTime;
+		//Positions[i * 4 + 1] += FVector(0, -0.5, +0.5) * DeltaTime;
+		//Positions[i * 4 + 2] += FVector(0, +0.5, -0.5) * DeltaTime;
+		//Positions[i * 4 + 3] += FVector(0, +0.5, +0.5) * DeltaTime;
+	}
+
+	//Positions[0] += FVector(0, 0, 1) * 100.f * DeltaTime;
+	ProcMesh->UpdateMeshSection(0, Positions, Normals, UVs, Colors, TArray<FProcMeshTangent>());
 }
 
 void ACVertexMesh::AddIndices(int32 InStart)
 {
-
+	Indices.Add(InStart + 2);
+	Indices.Add(InStart + 1);
+	Indices.Add(InStart + 0);
+	
+	Indices.Add(InStart + 3);
+	Indices.Add(InStart + 1);
+	Indices.Add(InStart + 2);
 }
